66914b808af20d79fe6d93a646330e685e693053
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-22 13:16:25 +0100

    build signup component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import Header from './Header';
+import Signup from './Users/Signup';
+
+import '../styles/main.css';
 
 class App extends Component {
 
   render() {
     return (
-      <div>Hello World</div>
+      <div className="container-fluid">
+        <Header />
+        <Signup />
+      </div>
     );
   }
 }
--- client/components/Header/index.jsx
+++ client/components/Header/index.jsx
@@ -4,7 +4,20 @@ class Header extends Component {
 
   render() {
     return (
-      <div>Header</div>
+      <nav className="navbar navbar-expand-lg navbar-dark header-bg-color">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a className="navbar-brand bold" href="#">Git Cheat Sheet</a>
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          
+        </ul>
+          <div className="">
+            
+          </div>
+      </div>
+    </nav>
     );
   }
 }
