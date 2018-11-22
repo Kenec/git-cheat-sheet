314b246aa8848a6fd2d5ad5c1985243a3b0968a9
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-22 14:49:36 +0100

    create 404 page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Header';
 import Users from './Users';
+import NotFound from './NotFound';
 
 import '../styles/main.css';
 
@@ -14,8 +15,9 @@ class App extends Component {
           <Header />
           <Switch>
             <Route exact path="/" component={Users} />
-            <Route exact path="/signin" component={Users} />
-            <Route exact path="/signup" component={Users} />
+            <Route path="/signin" component={Users} />
+            <Route path="/signup" component={Users} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </BrowserRouter>
