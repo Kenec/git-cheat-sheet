332d31df1a16bb90612ef9b24b5c6d5a98300079
Kenec <nnamani.kenechukwu@gmail.com>
2018-11-28 10:48:54 +0100

    setup mock data

--- client/reducers/initialState.js
+++ client/reducers/initialState.js
@@ -1,13 +1,50 @@
 export default {
   users: {},
   gitCheats: [
-    { id: 1, 
+    { id: 1,
+      owner: 0,
       title: 'Install Git',
-      Detail: { Description: [ 'Description 1', 'Description 2' ], Command: [ 'brew install git', 'sudo apt-get git' ] }
+      detail: [ 
+          { description: 'Description 1', command: 'brew install git' }, 
+          { description: 'Description 2', command: 'sudo apt-get git' },
+          { description: 'Description 3', command: 'sudo yum git' } 
+      ]
     },
-    { id: 2, 
+    { id: 2,
+      owner: 0,
       title: 'Clone Git Repo',
-      Detail: { Description: [ 'Description 1' ], Command: [ 'git clone repo-name' ] }
+      detail: [ { description: 'Description 1', command: 'git clone repo-name' } ]
+    },
+    { id: 3,
+      owner: 0,
+      title: 'Push to Git',
+      detail: [ 
+          { description: 'Description 1', command: 'git push origin branch-name' }
+      ]
+    },
+    { id: 4,
+      owner: 1, 
+      title: 'Custom Git Owner 1',
+      detail: [ 
+          { description: 'Description 1', command: 'git custom 1' }, 
+          { description: 'Description 2', command: 'git custom 2' },
+          { description: 'Description 3', command: 'git custom 3' },
+          { description: 'Description 4', command: 'git custom 4' }, 
+          { description: 'Description 5', command: 'git custom 5' },
+          { description: 'Description 6', command: 'git custom 6' } 
+      ]
+    },
+    { id: 4,
+      owner: 2, 
+      title: 'Custom Git Owner 2',
+      detail: [ 
+          { description: 'Description 1', command: 'git custom 1' }, 
+          { description: 'Description 2', command: 'git custom 2' },
+          { description: 'Description 3', command: 'git custom 3' },
+          { description: 'Description 4', command: 'git custom 4' }, 
+          { description: 'Description 5', command: 'git custom 5' },
+          { description: 'Description 6', command: 'git custom 6' } 
+      ]
     }
   ]
 }
