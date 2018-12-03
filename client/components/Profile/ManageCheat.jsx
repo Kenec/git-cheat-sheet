8e94b883a3416e23c54db7b79cead2fac17ced7d
Kenec <nnamani.kenechukwu@gmail.com>
2018-12-03 15:52:11 +0100

    Add cheat command and description form

--- client/components/Profile/ManageCheat.jsx
+++ client/components/Profile/ManageCheat.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ManageCheatForm from './ManageCheatForm';
 
 class ManageCheat extends Component {
 
@@ -7,33 +8,7 @@ class ManageCheat extends Component {
       <div className="container mt-3">
         <h4 className="custom-h4">Add Cheat</h4>
         <div className="card p-4">
-          <form>
-            <div className="form-group">
-              <label htmlFor="cheatTitle" className="col-form-label">Cheat Title</label>
-              <div className="">
-                <input type="text" className="form-control" id="cheatTitle" placeholder="Cheat Title eg: Install GIT" />
-              </div>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="cheatDesc1" className="col-form-label">Cheat Description 1</label>
-              <div className="">
-                <input type="text" className="form-control" id="cheatDesc1" placeholder="Cheat Description" />
-              </div>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="cheatCommand" className="col-form-label">Cheat Command 1</label>
-              <div className="">
-                <input type="text" className="form-control" id="cheatCommand" placeholder="Cheat Command eg: brew install git" />
-              </div>
-            </div>
-
-            <button type="submit" className="btn button-blend">Submit Cheat</button>
-            <hr className="hr-light"/>
-            <em>To add another cheat description and cheat command to this cheat title, click this button</em>
-            <button type="button" className="ml-2 btn btn-danger btn-round">+</button>
-          </form>
+          <ManageCheatForm />
         </div>
       </div>
     );
